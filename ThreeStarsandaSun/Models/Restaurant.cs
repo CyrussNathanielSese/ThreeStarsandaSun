a417c32802e1a1fe885133a10f4c0b90f7c4815c
Cyruss Sese <ac117370@avcol.school.nz>
2022-06-29 11:45:54 +1200

    added Restaurant Model

---
+++ ThreeStarsandaSun/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThreeStarsandaSun.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+
+        [Display(Name = "Name")]
+        [Required]
+        public string RestaurantName { get; set; }
+
+        [Display(Name = "Address")]
+        [Required]
+        public string RestaurantAddress { get; set; }
+
+        [Display(Name =" Number")]
+        public int RestaurantNum { get; set; }
+
+        [Required]
+        public City City { get; set; }
+
+    }
+}
