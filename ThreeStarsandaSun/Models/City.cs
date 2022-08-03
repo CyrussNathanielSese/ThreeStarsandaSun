34daf4e2d0ccc9ca76692923b4f12ddfa463b750
Cyruss Sese <ac117370@avcol.school.nz>
2022-08-04 10:25:57 +1200

    Deleted every classes and pages connected to it.

--- ThreeStarsandaSun/Models/City.cs
+++ ThreeStarsandaSun/Models/City.cs
@@ -1,17 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ThreeStarsandaSun.Models
+﻿namespace ThreeStarsandaSun.Models
 {
     public class City
     {
-        public int CityID { get; set; }
-        
-        [Display(Name = "City")]
-        [Required]
-        public string CityName { get; set; }
-
-
-        //public ICollection<Restaurant> Restaurants { get; set; }
-        //public ICollection<Event> Events { get; set; }
+        publi
     }
 }
--- ThreeStarsandaSun/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ThreeStarsandaSun.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
